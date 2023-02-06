310fac8f770ddb78ad7c076a54b5b4f729d0f844
timohuennebeck <hello@timohuennebeck.com>
2023-02-06 08:10:15 +0100

    add translation for chatbot

--- src/components/LiveChatMessage/LiveChatMessage.jsx
+++ src/components/LiveChatMessage/LiveChatMessage.jsx
@@ -2,8 +2,31 @@ import "./LiveChatMessage.scss";
 
 // images
 import profileImg from "../../assets/images/personal-profile.jpg";
+import { useState } from "react";
 
 export default function LiveChatMessage({ userMessage }) {
+    const [showTranslation, setShowTranslation] = useState(false);
+    const [translatedMessage, setTranslatedMessage] = useState("");
+
+    const deepLTranslation = () => {
+        fetch(
+            `https://api-free.deepl.com/v2/translate?auth_key=${process.env.REACT_APP_DEEPL_KEY}&text=${userMessage.message}&target_lang=EN`
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setShowTranslation(!showTranslation);
+                setTranslatedMessage(data.translations[0].text);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
     return (
         <div className="messages">
             <div className="messages__profile">
@@ -11,7 +34,9 @@ export default function LiveChatMessage({ userMessage }) {
             </div>
             <div className="messages__content">
                 <p className="messages__content-name">You</p>
-                <p className="messages__content-text">{userMessage.message}</p>
+                <p className="messages__content-text" onClick={deepLTranslation}>
+                    {showTranslation ? translatedMessage : userMessage.message}
+                </p>
             </div>
         </div>
     );
--- src/components/LiveChatMessageDifferentUser/LiveChatMessageDifferentUser.jsx
+++ src/components/LiveChatMessageDifferentUser/LiveChatMessageDifferentUser.jsx
@@ -2,8 +2,31 @@ import "./LiveChatMessageDifferentUser.scss";
 
 // images
 import profileImg from "../../assets/images/emma-profile.jpg";
+import { useState } from "react";
 
 export default function LiveChatMessageDifferentUser({ openaiMessage }) {
+    const [showTranslation, setShowTranslation] = useState(false);
+    const [translatedMessage, setTranslatedMessage] = useState("");
+
+    const deepLTranslation = () => {
+        fetch(
+            `https://api-free.deepl.com/v2/translate?auth_key=${process.env.REACT_APP_DEEPL_KEY}&text=${openaiMessage.message}&target_lang=EN`
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setShowTranslation(!showTranslation);
+                setTranslatedMessage(data.translations[0].text);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
     return (
         <div className="messages-different">
             <div className="messages-different__profile">
@@ -12,8 +35,11 @@ export default function LiveChatMessageDifferentUser({ openaiMessage }) {
             <div className="messages-different__content">
                 <p className="messages-different__content-name">Emma (GPT3)</p>
                 <div className="messages-different__content-container">
-                    <p className="messages-different__content-container-text">
-                        {openaiMessage.message}
+                    <p
+                        className="messages-different__content-container-text"
+                        onClick={deepLTranslation}
+                    >
+                        {showTranslation ? translatedMessage : openaiMessage.message}
                     </p>
                 </div>
             </div>
