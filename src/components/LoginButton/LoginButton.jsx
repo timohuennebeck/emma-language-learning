759f947ca53aa3dab622bf9ec148b090ca78eb14
timohuennebeck <hello@timohuennebeck.com>
2023-02-06 10:25:41 +0100

    refactor landing page

--- src/components/LoginButton/LoginButton.jsx
+++ src/components/LoginButton/LoginButton.jsx
@@ -1,9 +1,16 @@
+import "./LoginButton.scss";
+
+// librares
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
 
-    return <button onClick={() => loginWithRedirect()}>Log In</button>;
+    return (
+        <button className="login-button" onClick={() => loginWithRedirect()}>
+            Log In
+        </button>
+    );
 };
 
 export default LoginButton;
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,67 +1,42 @@
 import "./LandingPage.scss";
 
-import WhiteLogo from "../../assets/images/emma-logo.png";
-import AiGif from "../../assets/images/ai-face.gif";
+import logoImg from "../../assets/images/emma-logo.png";
+import aiImg from "../../assets/images/ai-face.gif";
 import LoginButton from "../../components/LoginButton/LoginButton";
 
 export default function LandingPage() {
-  return (
-    <div className="landing">
-      <div className="landing__logo">
-        <img className="landing__logo--img" src={WhiteLogo} alt="Emma Logo" />
-        <h3 className="landing__logo--copy">Emma</h3>
-      </div>
-
-      <div className="landing__nav">
-        <div className="landing__nav--left">
-          <a className="landing__nav--link" href="/emma">
-            Talk to Emma (AI)
-          </a>
+    return (
+        <div className="landing">
+            <div className="landing--bg">
+                <div className="landing__left">
+                    <div className="landing__left-logo">
+                        <img className="landing__left-logo-img" src={logoImg} alt="" />
+                    </div>
+                    <div className="landing__left-ai">
+                        <img className="landing__left-ai-img" src={aiImg} alt="" />
+                    </div>
+                    <h1 className="landing__left-header">
+                        Experience the{" "}
+                        <span className="landing__left-header-span medium-line">future</span> of{" "}
+                        <span className="landing__left-header-span medium-line">AI-powered</span>{" "}
+                        language learning with real-time{" "}
+                        <span className="new-line medium-line">conversations</span> and{" "}
+                        <span className="landing__left-header-span medium-line">translation</span>.
+                    </h1>
+                    <p>
+                        Emma, the advanced AI language tutor that can practice real-time
+                        conversations. Prefer a a human touch? Schedule a lesson with one of our
+                        tutors from around the world.
+                    </p>
+                    <div className="landing__left-button">
+                        <LoginButton />
+                        <button className="landing__left-button-signup">Sign Up</button>
+                    </div>
+                </div>
+                <div className="landing__right-ai">
+                    <img className="landing__right-ai-img" src={aiImg} alt="" />
+                </div>
+            </div>
         </div>
-
-        <div className="landing__nav--centre">
-          <a className="landing__nav--link" href="/tutor/:id">
-            Find a Tutor
-          </a>
-        </div>
-
-        <div className="landing__nav--middle">
-          <a className="landing__nav--link" href="/tutor/:id">
-            Become a Tutor
-          </a>
-        </div>
-      </div>
-
-      <div className="landing__avatar">
-        <img className="landing__avatar--gif" src={AiGif} alt="AI Gif" />
-      </div>
-
-      <div className="landing__title">
-        <h1 className="landing__title--text">
-          Experience the <span className="text__green-gradient">future</span> of{" "}
-          <span className="text__blue-gradient">AI-powered</span> language
-          learning with real-time
-          <span className="text__mix-gradient"> conversations</span>
-        </h1>
-      </div>
-
-      <div className="landing__copy">
-        <p className="landing__copy--text">
-          Emma, the advanced <b>AI language</b> tutor that can practice{" "}
-          <b>real-time conversations</b>. Prefer a a human touch? Schedule a
-          lesson with one of our tutors from around the world.
-        </p>
-      </div>
-
-      <div className="landing__buttons">
-        <LoginButton />
-        {/* <button className="landing__buttons--log-in" type="button">
-          <h3>Log In</h3>
-        </button> */}
-        <button className="landing__buttons--sign-up" type="button">
-          <h3>Sign Up</h3>
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
